497f632220c81cff3dcd535b86a7dd626a49f55f
Hong <Hong@138.195.106.200>
2015-03-25 09:13:48 +0100

    IHMcible version finale pour la soutenance

--- src/main/java/com/projetkm/GenericResource.java
+++ src/main/java/com/projetkm/GenericResource.java
@@ -23,7 +23,6 @@ import javax.ws.rs.core.MediaType;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-
 /**
  * REST Web Service
  *
@@ -43,50 +42,55 @@ public class GenericResource {
 
     /**
      * Retrieves representation of an instance of tweets.GenericResource
+     *
      * @return an instance of java.lang.String
      * @throws java.net.URISyntaxException
-     * 
+     *
      */
     @GET
     @Path("all")
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-    
+
     public String Getall() throws URISyntaxException {
-        
+
         return RepoData.Getall();
     }
-      @GET
+
+    @GET
     @Path("allLables")
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-    
+
     public String GetallLables() throws URISyntaxException {
-        
+
         return RepoData.GetallLabels();
     }
+
     @GET
     @Path("{type}")
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-    
+
     public String GetByType(@PathParam("type") String type) throws URISyntaxException {
-        
+
         return RepoData.GetType(type);
     }
-     @GET
+
+    @GET
     @Path("{type}/connections")
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-    
+
     public String GetElementConnection(@PathParam("type") String type) throws URISyntaxException {
-        
+
         return RepoData.GetElementConnection(type);
     }
+
     @GET
     @Path("{class}/{instance}")
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-    public String GetByTypeConnection(@PathParam("class") String cla,@PathParam("instance") String ins) throws URISyntaxException {
-        
-        return RepoData.GetConIns(cla,ins);
+    public String GetByTypeConnection(@PathParam("class") String cla, @PathParam("instance") String ins) throws URISyntaxException {
+
+        return RepoData.GetConIns(cla, ins);
     }
-    
+
     @GET
     @Path("{instance1}/{class}/{instance2}")
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
@@ -102,33 +106,112 @@ public class GenericResource {
         } else if (mClass == null) {
             return RepoData.GetInClaInIn(ins1, cla, ins2, mInstance);
         } else {
-           throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         }
     }
-     @GET
-    @Path("chef/definition")
+
+    @GET
+    @Path("chef de/definition")
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-    
+
     public String chef(@PathParam("type") String type) throws URISyntaxException {
-        
+
         return "jfjfjsqfjkjsmfjsdklgjsdlkjglksdgklsjgklsjdm";
     }
-    
-}
-    
-   
-    
-   /* @GET
-    @Path("{first}/{second}/{third}")
-    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-    public String GetElement(@PathParam("first") String first,
-                            @PathParam("second") String second,
-                            @PathParam("third") String third,
-                            @QueryParam("type") String type
-    ) throws URISyntaxException {
-        System.out.println(first);
-        
-        return RepoData.GetConnConnIn(first.replace("+", ""),second,third,type);
-    }*/
-   
 
+    @GET
+    @Path("chef de projet/Définition")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+
+    public String chef1(@PathParam("type") String type) throws URISyntaxException {
+
+        return "{\"data\":\"Définit, met en oeuvre et conduit un projet SI depuis sa conception jusqu'à la réception dans le but d'obtenir un résultat optimal et conforme aux exigences formulées par le chef de projet MOA ou le client métier en ce qui concerne la qualité, les performances, le coût, le délai et la sécurité. \"}";
+    }
+
+    @GET
+    @Path("chef de projet/Responsabilités")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+
+    public String chef2(@PathParam("type") String type) throws URISyntaxException {
+
+        return "{\"data\":\"Définit la conception technique et rédige les spécifications techniques détaillées, Participe au choix de progiciels, en relation avec le maître d’ouvrage Participe à la réalisation en termes de développements spécifiques ou d intégration Définit les tests et participe aux recettes \"}";
+
+    }
+
+    @GET
+    @Path("chef de projet/EDF")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+
+    public String chef3(@PathParam("type") String type) throws URISyntaxException {
+
+        return "{\"data\":\"Dans le cadre de votre mission, vous pilotez des projets télécoms ou des lots télécoms sur des projets infrastructures métiers, de type déploiement, coordination et intégration, avec une obligation de performance globale et de respect des contraintes projets (coûts, délais, ressources).\"}";
+    }
+
+    @GET
+    @Path("chef de projet/Mines Paris Tech")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+
+    public String chef4(@PathParam("type") String type) throws URISyntaxException {
+
+        return "{\"data\":\"MINES ParisTech forme depuis sa création en 1783 des ingénieurs de très haut niveau capables de résoudre des problèmes complexes dans des champs très variés\"}";
+    }
+
+    @GET
+    @Path("Energie/Description")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+
+    public String chef5(@PathParam("type") String type) throws URISyntaxException {
+
+        return "{\"data\":\"L'énergie est définie en physique comme la capacité d'un système à produire un travail1, entraînant un mouvement ou produisant par exemple de la lumière, de la chaleur ou de l’électricité. C'est une grandeur physique qui caractérise l'état d'un système et qui est d'une manière globale conservée au cours des transformations. L'énergie s'exprime en joules (dans le système international d'unités) ou souvent en kilowatt-heure (kW•h ou kWh).\"}";
+    }
+
+    @GET
+    @Path("administrateur outils/Définition")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+
+    public String chef6(@PathParam("type") String type) throws URISyntaxException {
+
+        return "{\"data\":\"Il installe, met en production, administre et exploite les moyens informatiques d’un ou plusieurs sites informatiques.Il participe au bon fonctionnement des systèmes d’information en garantissant le maintien à niveau des différents outils et/ou infrastructures des logiciels systèmes et/ou infrastructures de communication (locale, étendue, voix, image, architecture centralisée ou client-serveur), dans un objectif de qualité, de productivité et de sécurité. \"}";
+    }
+    @GET
+    @Path("administrateur outils/Responsabilités")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+
+    public String chef7(@PathParam("type") String type) throws URISyntaxException {
+
+       return "{\"data\":\"Est responsable du fonctionnement optimal des outils, systèmes ou réseaux dont il a la charge.Met en oeuvre les outils garantissant la cohérence des données \"}";
+    }
+    @GET
+    @Path("administrateur outils/EDF")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+
+    public String chef8(@PathParam("type") String type) throws URISyntaxException {
+
+         return "{\"data\":\"objectif de ce post est d'étudier la solution de gestion de ressource open source SLURM, afin évaluer la pertinence de sa mise en oeuvre sur les clusters de calcul de EDF, notamment pour ses fonctionnalités de gestion multi-clusters.\"}";
+    }
+    @GET
+    @Path("chef de projet/Energie")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+
+    public String chef9(@PathParam("type") String type) throws URISyntaxException {
+
+        return "{\"data\":\"Le chef de projet élabore la proposition technique à remettre au client sur un projet d’installation"
+                + " (centrale électrique, parc éolien, éclairage…)."
+                + " Il en assure la responsabilité technique et commerciale, en conformité avec les exigences du contrat."
+                + " Il recherche le meilleur compromis entre la satisfaction du client et le respect des objectifs et de la stratégie de l’entreprise.\"}";
+    }
+
+}
+
+/* @GET
+ @Path("{first}/{second}/{third}")
+ @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+ public String GetElement(@PathParam("first") String first,
+ @PathParam("second") String second,
+ @PathParam("third") String third,
+ @QueryParam("type") String type
+ ) throws URISyntaxException {
+ System.out.println(first);
+        
+ return RepoData.GetConnConnIn(first.replace("+", ""),second,third,type);
+ }*/
